9e84833c4cf68f51dc50c3a558ba1b25ff0eac26
kimjiwoons <bmw4518@gmail.com>
2026-09-04 22:47:27 +0900

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,4 @@
+const C="wordchain-pwa-neon-v1";
+self.addEventListener("install",e=>{self.skipWaiting()});
+self.addEventListener("activate",e=>{e.waitUntil(self.clients.claim())});
+self.addEventListener("fetch",e=>{if(e.request.method==="GET")e.respondWith(fetch(e.request).catch(()=>caches.match(e.request)))});
